9fd78c903685a9d7d7703ad8456656690dfd2468
tsmsp <tsm_sp@hotmail.com>
2022-08-16 19:09:55 -0700

    building routes and customizing firebase.

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import React, {useEffect} from 'react';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
+import { BlurView } from 'expo-blur';
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { firebaseConfig } from './config/firebase-config';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+
 import {API_URL, API_TOKEN} from "@env"
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>app-navigation</Text>
       <StatusBar style="auto" />
     </View>
   );
